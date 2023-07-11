3bdcc5904c97a75c183ed20e6a3c80534ae8c0d9
codeme254 <otwomadennis365@gmail.com>
2023-07-11 06:34:49 +0300

    organize files

---
+++ backend/controllers/article.controller.js
@@ -0,0 +1,89 @@
+const Article = require("../models/Article");
+const { emailAddressExists } = require("../utils/utils");
+
+module.exports = {
+  getAllArticles: async (req, res) => {
+    try {
+      const articles = await Article.find();
+      if (articles.length === 0) {
+        return res.status(204).json(articles);
+      } else {
+        return res.status(200).json(articles);
+      }
+    } catch (e) {
+      return res.status(500).json({ message: e.message });
+    }
+  },
+
+  createArticle: async (req, res) => {
+    try {
+      let { title, description, article_image, text, author } = req.body;
+      if (await emailAddressExists(author)) {
+        const article = new Article({
+          title,
+          description,
+          article_image,
+          text,
+          author,
+        });
+        const newArticle = await article.save();
+        newArticle.save();
+        return res.status(200).json(newArticle);
+      } else {
+        res.status(404).json({
+          message: "Not Allowed, email address of author not recognized",
+        });
+      }
+    } catch (e) {
+      return res.status(500).json({ message: e.message });
+    }
+  },
+
+  getSpecificArticle: async (req, res) => {
+    try {
+      const article = await Article.findById(req.params.id);
+      if (!article)
+        return res.status(404).json({ message: "Article not found" });
+      res.status(200).json(article);
+    } catch (e) {
+      return res.status(500).json({ message: e.message });
+    }
+  },
+
+  updateArticle: async (req, res) => {
+    const article = await Article.findOne({ _id: req.params.id });
+    if (!article) return res.status(404).json({ message: "Article not found" });
+    try {
+      // update title, description, image, text
+      const { title, description, image, text } = req.body;
+      if (title) {
+        article.title = title;
+      }
+      if (description) {
+        article.description = description;
+      }
+      if (image) {
+        article.image = image;
+      }
+      if (text) {
+        article.text = text;
+      }
+      await article.save();
+      return res.status(200).json({ message: "Article updated successfully" });
+    } catch (e) {
+      return res.status(500).json({ message: e.message });
+    }
+  },
+
+  deleteArticle: async (req, res) => {
+    try {
+      const article = await Article.findById(req.params.id);
+      if (!article)
+        return res.status(404).json({ message: "Article not found" });
+      await Article.deleteOne({ _id: req.params.id });
+      return res.status(200).json({ message: "Deleted successfully" });
+    } catch (e) {
+      return res.status(500).json({ message: e.message });
+    }
+  },
+};
